4d1f99890e978ed7735200eeffac2342d8779b42
Freza Majithia <frezamajithia@mac.home>
2025-11-17 18:17:15 -0500

    Initial Flask API with tests

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import app
+
+
+def test_hello():
+    client = app.app.test_client()
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Hello, World!"}
