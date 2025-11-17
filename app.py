dfc0924decfb9f2700f3b33df3fa2b3a37271036
Freza Majithia <frezamajithia@mac.home>
2025-11-17 18:57:07 -0500

    Final Version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return jsonify(message="Hello, World!")
 
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify(data), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
